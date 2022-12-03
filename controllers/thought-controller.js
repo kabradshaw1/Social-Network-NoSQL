b2f49803bd9785bdf0d24d75da007a79c5934b1b
kabradshaw1 <itllbok82@gmail.com>
2022-12-03 14:44:10 -0500

    roughly all controllers in place

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -37,7 +37,21 @@ const thoughtController = {
       })
       .catch(err => res.json(err));
   },
-
+  updateThought({ params, body }, res) {
+    Thought.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true
+    })
+      .then(dbThoughtData => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: 'No thought found with this id!' });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch(err => res.json(err));
+  },
+  
   // add Reaction to Thought
   addReaction({ params, body }, res) {
     Thought.findOneAndUpdate(
